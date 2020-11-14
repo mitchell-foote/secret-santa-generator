93cafc90ff80efbcdbe7d0e3f3e1af841d7a5e11
Mitchell Foote <mitchellfoote93@gmail.com>
2020-11-14 14:00:16 -0700

    Added randomize.js

---
+++ index.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+const randomizeAndAssign = require('./src/randomize');
+
+console.log(process.env.GMAIL_EMAIL);
---
+++ src/randomize.js
@@ -0,0 +1,42 @@
+const RandomGen = require('seed-random')
+
+
+module.export = function randomizeAndAssign(personList, seed) {
+    let shuffledPersonList = shuffle(personList, seed);
+    return shuffledPersonList.map((each, index) => {
+        let person = {...each};
+        if(index === shuffledPersonList.length -1) {
+            person.gifter = shuffledPersonList[0].email;
+        }
+        else {
+            person.gifter = shuffledPersonList[index + 1].email;
+        }
+        return person;
+    });
+}
+
+function shuffle(array, seed) {
+    let currentIndex
+    let temporaryValue
+    let randomIndex
+    let random
+    if(!seed) {
+        random = RandomGen(new Date().toISOString())
+    }
+    else {
+        random = RandomGen(seed.toString())
+    }
+    if(!Array.isArray(array)) {
+        throw new Error('You need to pass an array')
+    }
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(rand() * (currentIndex --))
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temporaryValue
+    };
+    return array;
+}
