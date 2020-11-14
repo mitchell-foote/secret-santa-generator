62e75a11d279f4418a2fd3c1c027026116bf7a30
Mitchell Foote <mitchellfoote93@gmail.com>
2020-11-14 13:28:13 -0700

    initial commit

---
+++ personList.js
@@ -0,0 +1,9 @@
+export default { 
+    personList: [
+        {name: "Batman", email: 'bw@wayne.net'},
+        {name: "Batman", email: 'bw@wayne.net'},
+        {name: "Batman", email: 'bw@wayne.net'},
+        {name: "Batman", email: 'bw@wayne.net'},
+        {name: "Batman", email: 'bw@wayne.net'},
+    ]
+}
